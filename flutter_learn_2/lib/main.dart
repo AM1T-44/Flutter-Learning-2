666526f66aa3d680b1a20e5985675c67b6ede0f6
AM1T-44 <u2204044@student.cuet.ac.bd>
2024-12-13 13:50:21 +0600

    Card Widget

---
+++ flutter_learn_2/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Hello, everyone'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Card(
+            elevation: 15,
+            shadowColor: Colors.amber.shade700,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                "Hello world",
+                style: TextStyle(fontSize: 25),
+              ),
+            )),
+      ),
+    );
+  }
+}
