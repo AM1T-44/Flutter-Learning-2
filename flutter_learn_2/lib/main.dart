c49487dde2603a68baa3c5da5dd9accb73c0bbcf
AM1T-44 <u2204044@student.cuet.ac.bd>
2024-12-25 23:57:14 +0600

    Stack Widget

--- flutter_learn_2/lib/main.dart
+++ flutter_learn_2/lib/main.dart
@@ -32,120 +32,65 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Column(
-          children: [
-            catItems(),
-            contact(),
-            subCatItems(),
-            bottomMenu(),
-          ],
-        ));
-  }
-}
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      // body: Container(
+      //   height: 300,
+      //   width: 300,
+      //   child: Stack(
+      //     children: [
+      //       Positioned(
+      //         child: Container(
+      //           height: 200,
+      //           width: 200,
+      //           color: Colors.red,
+      //         ),
+      //       ),
+      //       Positioned(
+      //         left: 25,
+      //         top: 25,
+      //         child: Container(
+      //           height: 200,
+      //           width: 200,
+      //           color: Colors.green,
+      //         ),
+      //       ),
+      //       Positioned(
+      //         left: 50,
+      //         top: 50,
+      //         child: Container(
+      //           height: 200,
+      //           width: 200,
+      //           color: Colors.blue,
+      //         ),
+      //       )
+      //     ],
+      //   ),
+      // ),
 
-class catItems extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      flex: 2,
-      child: Container(
-        color: Colors.red,
-        child: ListView.builder(
-          itemBuilder: (context, index) => Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SizedBox(
+      body: Stack(
+        children: [
+          Container(
+            height: 150,
+            width: 300,
+            color: Colors.green,
+          ),
+          Positioned(
+            left: 100,
+            top: 25,
+            child: Container(
+              height: 100,
               width: 100,
-              child: CircleAvatar(
-                backgroundColor: Colors.amber,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(50),
               ),
             ),
-          ),
-          itemCount: 8,
-          scrollDirection: Axis.horizontal,
-        ),
+          )
+        ],
       ),
     );
-  }
-}
-
-class contact extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-        flex: 4,
-        child: Container(
-          color: Colors.green,
-          child: ListView.builder(
-              itemBuilder: (context, index) => Padding(
-                    padding: EdgeInsets.all(8),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.amber,
-                      ),
-                      title: Text("Name"),
-                      subtitle: Text("Mobile"),
-                      trailing: Icon(Icons.delete),
-                    ),
-                  )),
-        ));
-  }
-}
-
-class subCatItems extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-        flex: 1,
-        child: Container(
-            color: Colors.blue,
-            child: ListView.builder(
-              itemBuilder: (context, index) => Padding(
-                padding: EdgeInsets.all(8),
-                child: Container(
-                  width: 200,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(11),
-                      color: Colors.amber),
-                ),
-              ),
-              itemCount: 10,
-              scrollDirection: Axis.horizontal,
-            )));
-  }
-}
-
-class bottomMenu extends StatelessWidget {
-  var arrColor = [
-    Colors.red,
-    Colors.orange,
-    Colors.green,
-    const Color.fromARGB(255, 0, 0, 0),
-    Colors.blue,
-    Colors.cyan,
-    Colors.purple,
-    Colors.pink,
-    Colors.grey,
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-        flex: 2,
-        child: Container(
-            color: Colors.yellow,
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3, crossAxisSpacing: 8, mainAxisSpacing: 8),
-              itemBuilder: (context, index) => Padding(
-                padding: EdgeInsets.all(11),
-                child: Container(
-                  color: arrColor[index],
-                ),
-              ),
-              itemCount: arrColor.length,
-            )));
   }
 }
