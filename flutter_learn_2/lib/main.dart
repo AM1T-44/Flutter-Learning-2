f0a7cd22b65bfcf4b33b376678f45cb276c4d2a0
AM1T-44 <u2204044@student.cuet.ac.bd>
2024-12-31 21:44:00 +0600

    Animate Container

--- flutter_learn_2/lib/main.dart
+++ flutter_learn_2/lib/main.dart
@@ -1,27 +1,61 @@
 import 'dart:async';
-import 'dart:io';
+import 'dart:collection';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter_learn_2/introPage.dart';
-import 'package:flutter_learn_2/widgets/rounded_btn.dart';
-import 'package:intl/intl.dart';
-
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<SplashScreen> createState() {
+    return _SplashScreen();
+  }
+}
+
+class _SplashScreen extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(
+      Duration(seconds: 0),
+      () {
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => MyHomePage(),
+            ));
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue.shade400,
+        child: Center(
+          child: Text(
+            'Animation Learning',
+            style: TextStyle(
+                fontSize: 25, fontWeight: FontWeight.w400, color: Colors.white),
+          ),
+        ),
+      ),
     );
   }
 }
@@ -29,32 +63,64 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() {
-    return MyHomePageState();
+    return _MyHomePageState();
   }
 }
 
-class MyHomePageState extends State<MyHomePage> {
-  RangeValues val = RangeValues(0, 10);
+class _MyHomePageState extends State<MyHomePage> {
+  var _height = 100.0;
+  var _width = 200.0;
+  bool toggle = true;
+  Decoration decor = BoxDecoration(
+      borderRadius: BorderRadius.circular(5), color: Colors.purple);
+
   @override
   Widget build(BuildContext context) {
-    RangeLabels labels = RangeLabels(val.start.toString(), val.end.toString());
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
-        backgroundColor: const Color.fromARGB(255, 180, 61, 210),
-        foregroundColor: Colors.white,
+        title: Text(
+          "Home Page",
+          style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.w400, color: Colors.white),
+        ),
+        backgroundColor: Colors.purple.shade600,
       ),
       body: Center(
-          child: RangeSlider(
-              values: val,
-              max: 10,
-              min: 0,
-              labels: labels,
-              divisions: 10,
-              onChanged: (newVal) {
-                val = newVal;
-                setState(() {});
-              })),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedContainer(
+              duration: Duration(seconds: 2),
+              height: _height,
+              width: _width,
+              decoration: decor,
+              curve: Curves.slowMiddle,
+            ),
+            SizedBox(
+              height: 11,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  if (toggle) {
+                    _height = 200.0;
+                    _width = 100.0;
+                    decor = BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.pink);
+                  } else {
+                    _height = 100.0;
+                    _width = 200.0;
+                    decor = BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.purple);
+                  }
+                  setState(() {});
+                  toggle ^= true;
+                },
+                child: Text('Animate'))
+          ],
+        ),
+      ),
     );
   }
 }
